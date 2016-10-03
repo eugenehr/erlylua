4af4922b4005dc1acb1ac15c67999c73f3054db1
eugenehr <eugene.khrustalev@gmail.com>
2016-10-03 19:25:40 +0300

    Travis CI integration

---
+++ .travis/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source .travis/platform.sh
+
+LUA_DIR="$TRAVIS_BUILD_DIR/lua$LUA"
+
+if [ $LUA == "5.3" ]
+then
+    curl http://www.lua.org/ftp/lua-5.3.3.tar.gz | tar xz
+    cd lua-5.3.3;
+fi
+
+make MYCFLAGS="-fPIC" MYLDFLAGS="-fPIC" TO_LIB="liblua$LUA.a" echo $PLATFORM
+make INSTALL_TOP="$LUA_DIR" install
+
+export LIBRARY_PATH="$LIBRARY_PATH:$LUA_DIR/lib"
+export LUA_INCLUDE_DIR=$LUA_DIR/include
+cd $TRAVIS_BUILD_DIR
+
+
+
